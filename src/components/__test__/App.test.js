aef0acb737b23eecb8fbe8e1d9e218f44f14578a
ariefirawan <ariefirawan@users.noreply.github.com>
2020-09-18 18:39:24 +0700

    jest testing manual

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -4,8 +4,11 @@ import ReactDom from 'react-dom';
 import App from '../App';
 
 it('show a comment box', () => {
-  const div = document.create('div');
+  const div = document.createElement('div');
 
   ReactDom.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Comment Box');
+
   ReactDom.unmountComponentAtNode(div);
 });
